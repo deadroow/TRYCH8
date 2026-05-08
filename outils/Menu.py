602428d20850fde420b96edcd8bd27a333a11269
Alexandre Waroux <alexwaroux84@gmail.com>
2026-05-08 19:16:25 +0200

    ajout du menu et de l'explorateur de fichier

---
+++ outils/Menu.py
@@ -0,0 +1,22 @@
+print("Je fonctionne")
+
+
+
+'''
+def main():
+    # On vérifie qu'un fichier ROM a été passé en argument
+    if len(sys.argv) < 2:
+        print("Usage: python emulator.py <fichier.ch8>")
+        sys.exit(1)
+
+    rompath = sys.argv[1]
+    print(f"Exécution de la ROM : {rompath}")
+
+    # On crée l'instance du CPU et on lance l'émulation
+    cpu = chip8()
+    cpu.run(rompath)
+
+
+if __name__ == '__main__':
+    main()
+'''
